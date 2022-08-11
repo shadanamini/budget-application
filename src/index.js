a5f5f10e1dc06d68aec724eb3717496e46d9b8e5
Shadan <shadanamini@gmail.com>
2022-08-10 22:25:05 -0700

    budget context and use local storage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BudgetsProvider} from './context/BudgetsContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BudgetsContext>
+      <App />
+    </BudgetsContext>
   </React.StrictMode>
 );
